bb1228aa8f6ed11a9864cd1dadd3727e149fcf18
lihqi <455711093@qq.com>
2022-11-23 17:29:39 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import moment from 'moment';
+import { Select, version } from 'antd';
+import 'antd/dist/antd.css';
+
+const Option = Select.Option;
+
+const Hello = () => (
+  <div style={{ margin: 10, overflow: 'scroll', height: 200 }}>
+    <h2>Select in a scrollable area</h2>
+    <div style={{ padding: 100, height: 1000, background: '#eee', position: 'relative' }} id="area">
+      <h4>可滚动的区域 / scrollable area</h4>
+      <Select
+        defaultValue="lucy"
+        style={{ width: 120 }}
+        getPopupContainer={() => document.getElementById('area')}
+      >
+        <Option value="jack">Jack</Option>
+        <Option value="lucy">Lucy</Option>
+        <Option value="yiminghe">yiminghe</Option>
+      </Select>
+    </div>
+  </div>
+);
+
+ReactDOM.render(
+  <Hello />,
+  document.getElementById('root')
+);
